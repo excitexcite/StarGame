f9c71e093ccf85401cbc51896e22ef5c28c7c47d
unknown <excitenew@gmail.com>
2020-12-02 03:12:22 +0200

    add scrolling background visual effect

---
+++ StarGame/Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    // скорость скрола фона
+    [SerializeField] float backgroundScrollSpeed = 0.2f;
+    // переменная для того, что бы связать модельку (метариал), который мы указали в инспекторе юнити с кодом
+    Material myMaterial;
+    // вектор для задания движения
+    Vector2 offSet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // получили материал фона
+        myMaterial = GetComponent<Renderer>().material;
+        // задали вектор
+        offSet = new Vector2(0f, backgroundScrollSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // по смене кадров двигаем фон на offSet 
+        myMaterial.mainTextureOffset += offSet * Time.deltaTime;
+    }
+}
