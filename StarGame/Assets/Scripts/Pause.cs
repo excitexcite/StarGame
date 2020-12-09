a5dd3615d532accdf1de45f7425108f19ba8552e
unknown <171258@nuos.edu.ua>
2020-12-10 00:33:35 +0200

    add pause button

---
+++ StarGame/Assets/Scripts/Pause.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+
+    private bool isPaused = false;
+
+    public void PauseGame()
+    {
+        if (isPaused)
+        {
+            Time.timeScale = 1;
+            AudioListener.pause = false;
+            isPaused = false;
+        } 
+        else
+        {
+            Time.timeScale = 0;
+            AudioListener.pause = true;
+            isPaused = true;
+        }
+    }
+}
