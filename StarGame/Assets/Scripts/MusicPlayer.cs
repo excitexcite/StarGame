dd876cd6a590d2ff884b232c74c9693d69018c5d
unknown <171258@nuos.edu.ua>
2020-12-09 23:22:04 +0200

    add background music

---
+++ StarGame/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        SetUpSingleton();
+    }
+
+    private void SetUpSingleton()
+    {
+        // если у нас более 1 трека музыки, то при смене экрана мы удалим первый трек
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        // иначе этот трек будет продолжать играть
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
